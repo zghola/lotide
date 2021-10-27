93270de579e429dd6973d4c31e90f57acccddb46
zinat <zinat.gholami@gmail.com>
2021-10-27 22:27:53 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,19 +1,46 @@
-// FUNCTION IMPLEMENTATION
 const eqArrays = function(arr1, arr2) {
-   
+    if(arr1.length !== arr2.length)
+    return false;
+
+    for(let i = 0; i < arr1.length; i+= 1){
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+    }
+    return true
+
+};
+const assertArraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+    console.log(`🛑 Assertion Failed: [${actual}] === [${expected}]`);
+    }
 };
 
-// TEST CODE
-console.log (eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+const middle = function(array) {
+    if(array.length <= 2 ){
+        return []
+    }
+    if(array.length % 2 === 0){
+        const index2 = array.length / 2
+        const index1 = index2-1
+        const value1 = array[index1]
+        const value2 = array[index2]
+        return [value1, value2]
+    } 
+    const index = (array.length -1) / 2  
+    const value = array[index]
+    return [value]
+}
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-const assertEqual = function(actual, expected) {
-   
-  };
-    
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  
+assertArraysEqual(middle([1, 2, 3]), [2] )
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3] )
+assertArraysEqual(middle([1]), [])
+assertArraysEqual(middle([1, 2]),[])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4])
