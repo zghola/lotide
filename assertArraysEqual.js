a61d6d78c83d32dc1c0cf293f06caadf7ba91ff5
zinat <zinat.gholami@gmail.com>
2021-10-27 18:26:29 +0000

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+    return false;
+
+    for(let i = 0; i < arr1.length; i+= 1){
+    if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+    }
+    return true
+
+};
+const assertArraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+    console.log(`🛑 Assertion Failed: [${actual}] === [${expected}]`);
+    }
+};
+assertArraysEqual([1,2,2,3], [4,4,3,6])
+assertArraysEqual([1,2,2,3], [1,2,2,3])
---
+++ middle.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(arr1, arr2) {
+   
+};
+
+// TEST CODE
+console.log (eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+const assertEqual = function(actual, expected) {
+   
+  };
+    
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
+  
