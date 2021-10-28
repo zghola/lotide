7600f4a07a3f0a38fc35450cf43fc72006a312af
zinat <zinat.gholami@gmail.com>
2021-10-28 00:33:53 +0000

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+   
+      if (actual === expected) {
+        console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
+      } else {
+        console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
+      }
+     
+  };
+    
+const countOnly = function(allItems, itemsToCount){
+    const results = {};
+
+    for (const item of allItems) { 
+        if (itemsToCount[item]){
+        
+            if (results[item]) {
+                results[item] += 1;
+            } else {
+                results[item] = 1;
+                
+            }
+        }
+    }
+    return results;
+} 
+
+
+ const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  console.log (result1)
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+  assertEqual(result1["Agouhanna"], undefined);
