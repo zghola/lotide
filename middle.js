183c4cb46669ea54f4fda2b46705e531e4f72686
zinat <zinat.gholami@gmail.com>
2021-11-01 21:56:43 +0000

    completed refactoring middle.js

--- middle.js
+++ middle.js
@@ -33,14 +33,4 @@ const middle = function(array) {
     const value = array[index]
     return [value]
 }
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3]), [2] )
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3] )
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]),[])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4])
+module.exports = middle
---
+++ test/middleTest.js
@@ -0,0 +1,15 @@
+const middle = require(`../middle`)
+const assertArraysEqual = require(`../assertArraysEqual`)
+
+
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+assertArraysEqual(middle([1, 2, 3]), [2] )
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3] )
+assertArraysEqual(middle([1]), [])
+assertArraysEqual(middle([1, 2]),[])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4])
