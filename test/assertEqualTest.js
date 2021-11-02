70fc874a1caa8718e276fb8791a96a6ce87f51d3
zinat <zinat.gholami@gmail.com>
2021-11-02 19:18:31 +0000

    exporting functions

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,18 +1,11 @@
-/*const assertEqual = require('../assertEqual');
+const assertEqual = require('../assertEqual');
 
 
 
 
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);*/
+assertEqual(1, 1);
 
-const assert = require('chai').assert;
-const assertEqual   = require('../assertEqual');
- 
-describe("#assertEqual", () => {
-  it("returns 1 for 1", () => {
-    assert.Equal(assertEqual ([1]), 1);
-  });
-});
+
 
 
